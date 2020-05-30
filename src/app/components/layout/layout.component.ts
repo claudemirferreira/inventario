d0fac926c90574fa344326d906fbbaa8435c4b36
claudemir <claudemirramosferreira@gmail.com>
2020-05-30 16:18:36 -0400

    ajuste no login

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -6,27 +6,38 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(map(result => result.matches));
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map((result) => result.matches));
 
   @ViewChild('panel', { static: true })
   private sidePanel: MatSidenav;
 
   @ViewChild('content', { static: true, read: ViewContainerRef })
   private vcf: ViewContainerRef;
+  shared: SharedService;
 
-  constructor(private breakpointObserver: BreakpointObserver, private sidenavService: SidenavService) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private sidenavService: SidenavService
+  ) {
+    this.shared = SharedService.getInstance();
+  }
 
   ngOnInit() {
     this.sidenavService.setPanel(this.sidePanel);
     this.sidenavService.setContentVcf(this.vcf);
   }
 
+  isLoggedIn(): boolean {
+    return this.shared.isLoggedIn();
+  }
 }
